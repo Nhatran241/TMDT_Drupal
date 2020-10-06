078c029cd678050c3233446863ea5ceeba7ebcbd
Tran Nhat <trannhat2411999@gmail.com>
2020-10-06 13:55:21 +0700

    search

---
+++ sites/default/files/php/twig/5f7aeb2239bcb_commerce-checkout-complet_vDDzxDKoKIzCxuf5K3QHJoPXy/XElK5P_Xk1bRlt1KAD_RtLX_u6OEHsFgzxfS5PCAMG8.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/commerce/modules/checkout/templates/commerce-checkout-completion-message.html.twig */
+class __TwigTemplate_ead7f9a802cc5dbb60f0ae150e9d2bf60e55bac54c58404df201f12854e9b845 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 17];
+        $filters = ["escape" => 15, "t" => 19];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 't'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 14
+        echo "<div class=\"checkout-complete\">";
+        // line 15
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["message"] ?? null)), "html", null, true);
+        // line 17
+        if (($context["payment_instructions"] ?? null)) {
+            // line 18
+            echo "    <div class=\"checkout-complete__payment-instructions\">
+      <h2>";
+            // line 19
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Payment instructions"));
+            echo "</h2>
+      ";
+            // line 20
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["payment_instructions"] ?? null)), "html", null, true);
+            echo "
+    </div>
+  ";
+        }
+        // line 23
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/commerce/modules/checkout/templates/commerce-checkout-completion-message.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  74 => 23,  68 => 20,  64 => 19,  61 => 18,  59 => 17,  57 => 15,  55 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/commerce/modules/checkout/templates/commerce-checkout-completion-message.html.twig", "C:\\xampp\\htdocs\\new\\modules\\contrib\\commerce\\modules\\checkout\\templates\\commerce-checkout-completion-message.html.twig");
+    }
+}
